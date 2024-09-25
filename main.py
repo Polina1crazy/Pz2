484c6278e193efa63fbc2f22c8ed60a6c4620fe0
Polina1crazy <bidion.911@gmail.com>
2024-09-25 10:40:27 +0300

    fixic

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def get_math_text(a, b,c):
     d = (b ** 2) - (4 * a * c)
     if (d < 0):
         return None
+    if (d == 0):
+        x = ((b * (-1)) + (d ** 0.5)) / (2 * a)
     else:
         x1 = ((b * (-1)) + (d ** 0.5)) / (2 * a)
         x2 = ((b * (-1)) - (d ** 0.5)) / (2 * a)
